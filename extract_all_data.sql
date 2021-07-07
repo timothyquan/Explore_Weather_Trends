77fd6bce55008508c6c9793982ec05eb25ed3300
timothyquan <48138393+timothyquan@users.noreply.github.com>
2021-07-07 21:06:40 +0700

    commited

---
+++ extract_all_data.sql
@@ -0,0 +1,13 @@
+-- just city data from bangkok:
+--------------------------------------------------------
+
+SELECT *
+FROM city_data cd
+WHERE city='Bangkok';
+--------------------------------------------------------
+
+-- just global_data:
+
+SELECT * FROM global_data
+--------------------------------------------------------
+
